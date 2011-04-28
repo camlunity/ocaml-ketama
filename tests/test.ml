0cb536b8635fcdf76aeb4c618c94cdacbb7a2f75
Roman Sokolov <sokolov.r.v@gmail.com>
2011-04-29 10:39:27 +1200

    improve test

--- tests/test.ml
+++ tests/test.ml
@@ -1,19 +1,31 @@
+
 open Printf
 
+open Ketama
+
+
 let dump_mcs m =
-  eprintf "point: %s, ip: %s\n" (Uint32.to_string m.point) m.ip
-let dump_server_info si =
-  eprintf "addr: %s, memory: %d\n" si.addr si.memory
+  printf "point: %s, ip: %s\n" (Uint32.to_string m.point) m.ip
 
 
-let test () =
-  let c = Ketama.create_continuum_from_file "ketama.servers" in
+let test filename =
+  let c = Ketama.create_continuum_from_file filename in
   let v = Ketama.search_server c "abcd" in
-    eprintf "total: %d\n" (Array.length c.array);
-    eprintf "hash: %s\n" (Uint32.to_string (hash "abcd"));
-    eprintf "found: %s\n" v.ip;
-    eprintf "maxint32: %nu\n" Int32.max_int;
+    printf "total: %d\n" (Array.length c.array);
+    printf "hash: %s\n" (Uint32.to_string (hash "abcd"));
+    printf "found: %s\n" v.ip;
+    printf "maxint32: %lu\n" Int32.max_int;
     Array.iter dump_mcs c.array
 
-let () = test ()
+
+let main () =
+  if Array.length Sys.argv < 2 then (
+    printf "no filename supplied. using tests/ketama.servers";
+    test "tests/ketama.servers"
+  )
+  else
+    test Sys.argv.(1)
+
+
+let () = main ()
 
