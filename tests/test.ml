d68dd4a8979a9f632d6210308bc8533a3c5b72d2
Roman Sokolov <sokolov.r.v@gmail.com>
2011-04-29 11:11:29 +1200

    use quickcheck for tests

--- tests/test.ml
+++ tests/test.ml
@@ -1,26 +1,33 @@
 
-open Printf
+open QuickCheck
 
 open Ketama
 
+let testable_string_to_bool =
+  testable_fun arbitrary_string show_string testable_bool
 
-let dump_mcs m =
-  printf "point: %s, ip: %s\n" (Uint32.to_string m.point) m.ip
-
+let cls = quickCheck testable_string_to_bool
 
 let test filename =
-  let c = Ketama.create_continuum_from_file filename in
-  let v = Ketama.search_server c "abcd" in
-    printf "total: %d\n" (Array.length c.array);
-    printf "hash: %s\n" (Uint32.to_string (hash "abcd"));
-    printf "found: %s\n" v.ip;
-    printf "maxint32: %lu\n" Int32.max_int;
-    Array.iter dump_mcs c.array
+  let c_many = create_continuum_from_file filename in
+  let c_one = create_continuum [| {addr="127.0.0.1"; memory=100} |] in
+  let prop_one_for_one s =
+    let mcs = get_server c_one s in
+    mcs.ip = "127.0.0.1"
+  in
+  let prop_ident_for_equal s =
+    let mcs1 = get_server c_many s in
+    let mcs2 = get_server c_many s in
+    mcs1 == mcs2
+  in
+  let () = cls prop_one_for_one in
+  let () = cls prop_ident_for_equal in
+  ()
 
 
 let main () =
   if Array.length Sys.argv < 2 then (
-    printf "no filename supplied. using tests/ketama.servers";
+    Printf.printf "no filename supplied. using tests/ketama.servers\n";
     test "tests/ketama.servers"
   )
   else
